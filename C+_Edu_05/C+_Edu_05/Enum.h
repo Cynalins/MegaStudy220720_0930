e33c6905faa3cc4cb7f9bb97ccca24fcdc097a33
Cyna <kjm981231@naver.com>
2022-08-30 11:25:46 +0900

    Scene_AddFunc

---
+++ C+_Edu_05/C+_Edu_05/Enum.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+enum class SceneID
+{
+	LOGO,
+	MENU,
+	STAGE,
+	EXIT
+};
+
---
+++ C+_Edu_05/C+_Edu_05/headers.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+#include "struct.h"
+#include "Enum.h"
+
+
